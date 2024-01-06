99a372198df98b7c84264f568a4905069865f2a7
Andréa <andrea.marnier@gmail.com>
2024-01-06 17:20:18 +0100

    2015 day 12

---
+++ 2015/day_12.py
@@ -0,0 +1,37 @@
+import argparse
+import json
+
+from pathlib import Path
+
+def _parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--part", "-p",
+        type=int,
+        choices={1, 2},
+        help="Set puzzle part"
+    )
+    args = parser.parse_args()
+    if not args.part:
+        parser.error("Which part are you solving?")
+    return args
+
+def find_sum(item: any, ignore_red: bool = False) -> int:
+    if isinstance(item, int):
+        return item
+    if isinstance(item, dict):
+        if ignore_red and "red" in item.values():
+            return 0
+        return sum([find_sum(val, ignore_red) for val in item.values()])
+    if isinstance(item, list):
+        return sum([find_sum(val, ignore_red) for val in item])
+    return 0
+
+if __name__ == "__main__":
+    args = _parse_args()
+    with Path(f"inputs/{Path(__file__).stem}.txt").open("r") as file:
+        data = file.read()
+    if args.part == 1:
+        print(find_sum(json.loads(data)))
+    else:
+        print(find_sum(json.loads(data), ignore_red=True))
