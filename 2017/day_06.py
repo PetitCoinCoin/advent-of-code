b683189015cf2cf8c5416eeaf498035401bb6c2e
Andréa <andrea.marnier@gmail.com>
2024-10-17 21:28:19 +0200

    2017 day 6

---
+++ 2017/day_06.py
@@ -0,0 +1,40 @@
+import argparse
+
+from pathlib import Path
+
+def _parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--part", "-p",
+        type=int,
+        choices={1, 2},
+        help="Set puzzle part"
+    )
+    args = parser.parse_args()
+    if not args.part:
+        parser.error("Which part are you solving?")
+    return args
+
+def redistribute(items: list) -> list:
+    max_idx = items.index(max(items))
+    nb_blocks = items[max_idx]
+    items[max_idx] = 0
+    items = [item + nb_blocks // len(items) for item in items]
+    for delta in range(1, nb_blocks % len(items) + 1):
+        items[(max_idx + delta) % len(items)] += 1
+    return items
+
+if __name__ == "__main__":
+    args = _parse_args()
+    with Path(f"inputs/{Path(__file__).stem}.txt").open("r") as file:
+        data = [int(x) for x in file.read().split("\t")]
+    seen = dict()
+    cpt = 0
+    while seen.get(str(data)) is None:
+        seen[str(data)] = cpt
+        data = redistribute(data)
+        cpt += 1
+    if args.part == 1:
+        print(cpt)
+    else:
+        print(cpt - seen.get(str(data)))
