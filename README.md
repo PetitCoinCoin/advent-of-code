f332bb1b97bb9d77689e97f5caabfa90aed30ee0
Andréa <andrea.marnier@gmail.com>
2024-12-02 08:11:05 +0100

    day 02

--- README.md
+++ README.md
@@ -2,4 +2,7 @@ Ready to find the Chief Historian: https://adventofcode.com/2024
 
           --------Part 1--------   --------Part 2--------
     Day       Time   Rank  Score       Time   Rank  Score
+      2   01:06:20  14899      0   02:07:55  14059      0
       1   02:35:21  15785      0   02:38:15  14712      0
+
+Wanted something clever for day 2 part 2, but went for bruteforce anyway because it's faster (well, for now).
--- day_02.py
+++ day_02.py
@@ -16,13 +16,32 @@ def _parse_args() -> argparse.Namespace:
         parser.error("Which part are you solving?")
     return args
 
+def is_safe(report: list) -> bool:
+    prev_d = None
+    for i in range(1, len(report)):
+        d = report[i] - report[i-1]
+        if not d or abs(d) > 3:
+            return False
+        if prev_d and prev_d * d < 0:
+            return False
+        prev_d = d
+    return True
+
+def is_really_safe(report: list) -> bool:
+    if is_safe(report):
+        return True
+    for i in range(len(report)):
+        if is_safe(report[:i] + report[i + 1:]):
+            return True
+    return False
+
 if __name__ == "__main__":
     args = _parse_args()
     t = time()
     with Path(f"inputs/{Path(__file__).stem}.txt").open("r") as file:
-        data = file.read()
+        data = [[int(x) for x in line.split(" ")] for line in file.read().split("\n")]
     if args.part == 1:
-        print(data)
+        print(sum(is_safe(report) for report in data))
     else:
-        raise NotImplementedError
+        print(sum(is_really_safe(report) for report in data))
     print(time() - t)
