fcf9466ca16aad864967b0cc02be4d9b0496f8ba
Andréa <andrea.marnier@gmail.com>
2024-12-04 06:47:10 +0100

    day 04

--- README.md
+++ README.md
@@ -2,6 +2,7 @@ Ready to find the Chief Historian: https://adventofcode.com/2024
 
           --------Part 1--------   --------Part 2--------
     Day       Time   Rank  Score       Time   Rank  Score
+      4   00:24:42   5111      0   00:33:23   3970      0
       3   00:10:36   4490      0   00:15:30   2437      0
       2   01:06:20  14899      0   02:07:55  14059      0
       1   02:35:21  15785      0   02:38:15  14712      0
--- day_04.py
+++ day_04.py
@@ -16,13 +16,47 @@ def _parse_args() -> argparse.Namespace:
         parser.error("Which part are you solving?")
     return args
 
+def parse_input(raw: str, i: int) -> None:
+    for r, char in enumerate(raw):
+        data[complex(r, -i)] = char
+
+def xmas(position) -> bool:
+    expected = "MAS"
+    delta = (1j, -1j, 1, -1, -1j -1, -1j + 1, 1j -1, 1j + 1)
+    i = 0
+    queue = {(d, position + d) for d in delta if expected[i] == data.get(position + d, ".")}
+    while queue and i < 2:
+        new_queue = set()
+        i += 1
+        for d, pos in queue:
+            if expected[i] == data.get(pos + d, "."):
+                    new_queue.add((d, pos + d))
+        queue = new_queue
+    return len(queue)
+
+
+def x_mas(position) -> int:
+    delta = (-1j -1, -1j + 1, 1j -1, 1j + 1)
+    queue = {position + d:  data.get(position + d, ".") for d in delta if data.get(position + d, ".") in "MS"}
+    if len(queue) < 4:
+        return 0
+    return int(queue[position -1j -1] != queue[position + 1j + 1] and queue[position -1j +1] != queue[position + 1j - 1])
+
 if __name__ == "__main__":
     args = _parse_args()
     t = time()
+    data = {}
     with Path(f"inputs/{Path(__file__).stem}.txt").open("r") as file:
-        data = file.read()
+        [parse_input(line, i) for i,line in enumerate(file.read().split("\n"))]
     if args.part == 1:
-        print(data)
+        pivot = "X"
+        func = xmas
     else:
-        raise NotImplementedError
+        pivot = "A"
+        func = x_mas
+    total = 0
+    for k, v in data.items():
+        if v == pivot:
+            total += func(k)
+    print(total)
     print(time() - t)
