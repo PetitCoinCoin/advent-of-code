d04d3966b768885ec2ae99e867837abbe74203cd
Andréa <andrea.marnier@gmail.com>
2024-12-06 12:00:14 +0100

    day 06

--- README.md
+++ README.md
@@ -2,6 +2,7 @@ Ready to find the Chief Historian: https://adventofcode.com/2024
 
           --------Part 1--------   --------Part 2--------
     Day       Time   Rank  Score       Time   Rank  Score
+      6   02:04:51  15057      0   03:38:38  11355      0
       5   01:23:25  13101      0   03:55:07  18792      0
       4   00:24:42   5111      0   00:33:23   3970      0
       3   00:10:36   4490      0   00:15:30   2437      0
@@ -9,3 +10,6 @@ Ready to find the Chief Historian: https://adventofcode.com/2024
       1   02:35:21  15785      0   02:38:15  14712      0
 
 Wanted something clever for day 2 part 2, but went for bruteforce anyway because it's faster (well, for now).
+
+Again, wanted something clever for day 6 part 2, but I didn't get the correct answer. So I went for pure bruteforce, which took me more than 3minutes (but I got the correct answer). Still, I'm sure something clever should work, so I reworked this. I'm now stuck with a "clever bruteforce": I just narrowed the number of possibilities, so it now takes around 7sec to run. I thought I could change the algorithm but realized there was several configurations that I couldn't anticipate as loops (well, at least I'm not able to do it).
+
--- day_06.py
+++ day_06.py
@@ -16,13 +16,67 @@ def _parse_args() -> argparse.Namespace:
         parser.error("Which part are you solving?")
     return args
 
+def parse_input(raw: str, i) -> None:
+    for r, char in enumerate(raw):
+        data[complex(r, -i)] = char
+
+def guard_move(is_part_two: bool = False) -> list | int:
+    position = start
+    seen = {start}
+    d = 1j
+    while True:
+        next_char = data.get(position + d, "")
+        if not next_char:
+            return seen if is_part_two else len(seen)
+        while next_char == "#":
+            d *= -1j
+            next_char = data.get(position + d, "")
+        position += d
+        seen.add(position)
+
+def guard_loop() -> int:
+    position = start
+    seen = set()
+    d = 1j
+    while True :
+        if (position, d) in seen:
+            return 1
+        next_char = data.get(position + d, "")
+        if not next_char:
+            return 0
+        if next_char == ".":
+            position += d
+            continue
+        while next_char == "#":
+            seen.add((position, d))
+            d *= -1j
+            next_char = data.get(position + d, "")
+        position += d
+
 if __name__ == "__main__":
     args = _parse_args()
     t = time()
+    data = {}
     with Path(f"inputs/{Path(__file__).stem}.txt").open("r") as file:
-        data = file.read()
+        i = 0
+        while line := file.readline():
+            parse_input(line.strip(), i)
+            i += 1
+    for k, v in data.items():
+        if v == "^":
+            start = k
+            data[k] = "."
+            break
     if args.part == 1:
-        print(data)
+        print(guard_move())
     else:
-        raise NotImplementedError
+        path = guard_move(True)
+        set_path = set(path)
+        set_path.remove(start)
+        obstacles = 0
+        for position in set_path:
+            data[position] = "#"
+            obstacles += guard_loop()
+            data[position] = "."
+        print(obstacles)
     print(time() - t)
