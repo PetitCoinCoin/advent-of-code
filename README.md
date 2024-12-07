a19e1b0c0f0ccbafa52ee073216dcab741c1ea8b
Andréa <andrea.marnier@gmail.com>
2024-12-07 12:06:44 +0100

    day 07

--- README.md
+++ README.md
@@ -2,6 +2,7 @@ Ready to find the Chief Historian: https://adventofcode.com/2024
 
           --------Part 1--------   --------Part 2--------
     Day       Time   Rank  Score       Time   Rank  Score
+      7   06:00:26  24115      0   06:04:53  21677      0
       6   02:04:51  15057      0   03:38:38  11355      0
       5   01:23:25  13101      0   03:55:07  18792      0
       4   00:24:42   5111      0   00:33:23   3970      0
@@ -13,3 +14,4 @@ Wanted something clever for day 2 part 2, but went for bruteforce anyway because
 
 Again, wanted something clever for day 6 part 2, but I didn't get the correct answer. So I went for pure bruteforce, which took me more than 3minutes (but I got the correct answer). Still, I'm sure something clever should work, so I reworked this. I'm now stuck with a "clever bruteforce": I just narrowed the number of possibilities, so it now takes around 7sec to run. I thought I could change the algorithm but realized there was several configurations that I couldn't anticipate as loops (well, at least I'm not able to do it).
 
+Lost so much time on Day 7 part 1 because I used a dict to store the input, but there were duplicate keys!
--- day_07.py
+++ day_07.py
@@ -1,5 +1,6 @@
 import argparse
 
+from collections import deque
 from pathlib import Path
 from time import time
 
@@ -16,13 +17,33 @@ def _parse_args() -> argparse.Namespace:
         parser.error("Which part are you solving?")
     return args
 
+OPERATORS = (
+    lambda x,y: x + y,
+    lambda x,y: x * y,
+)
+
+def can_be_made(val: int, eq: deque, *, is_part_two: bool = False) -> bool:
+    functions = OPERATORS + (lambda x, y: int(str(x) + str(y)),) if is_part_two else OPERATORS
+    res = eq.popleft()
+    queue = {res}
+    while eq:
+        next_val = eq.popleft()
+        new_queue = set()
+        for r in queue:
+            if r > val:
+                continue
+            for func in functions:
+                new_queue.add(func(r, next_val))
+        queue = new_queue
+    return val in queue
+
 if __name__ == "__main__":
     args = _parse_args()
     t = time()
+    data = []
     with Path(f"inputs/{Path(__file__).stem}.txt").open("r") as file:
-        data = file.read()
-    if args.part == 1:
-        print(data)
-    else:
-        raise NotImplementedError
+        while line := file.readline():
+            key, val = line.strip().split(":")
+            data.append((int(key), deque([int(x.strip()) for x in val.split(" ") if x])))
+    print(sum(key for key, value in data if can_be_made(key, value, is_part_two=args.part == 2)))
     print(time() - t)
