731fba549c6f412d3b8b4b8e09cd4601bb4e84ef
Andréa <andrea.marnier@gmail.com>
2024-12-08 07:45:07 +0100

    day 08

--- README.md
+++ README.md
@@ -2,6 +2,7 @@ Ready to find the Chief Historian: https://adventofcode.com/2024
 
           --------Part 1--------   --------Part 2--------
     Day       Time   Rank  Score       Time   Rank  Score
+      8   01:37:25   9351      0   01:40:57   8232      0
       7   06:00:26  24115      0   06:04:53  21677      0
       6   02:04:51  15057      0   03:38:38  11355      0
       5   01:23:25  13101      0   03:55:07  18792      0
@@ -15,3 +16,5 @@ Wanted something clever for day 2 part 2, but went for bruteforce anyway because
 Again, wanted something clever for day 6 part 2, but I didn't get the correct answer. So I went for pure bruteforce, which took me more than 3minutes (but I got the correct answer). Still, I'm sure something clever should work, so I reworked this. I'm now stuck with a "clever bruteforce": I just narrowed the number of possibilities, so it now takes around 7sec to run. I thought I could change the algorithm but realized there was several configurations that I couldn't anticipate as loops (well, at least I'm not able to do it).
 
 Lost so much time on Day 7 part 1 because I used a dict to store the input, but there were duplicate keys!
+
+I was afraid that it would take forever for Day 8 with permutations, but it runs in no time!
--- day_08.py
+++ day_08.py
@@ -1,5 +1,6 @@
 import argparse
 
+from itertools import permutations
 from pathlib import Path
 from time import time
 
@@ -16,13 +17,35 @@ def _parse_args() -> argparse.Namespace:
         parser.error("Which part are you solving?")
     return args
 
+def locate_antinodes(antennas: set, *, is_part_two: bool = False) -> set:
+    res = set()
+    for a, b in permutations(antennas, 2):
+        delta = b - a
+        anti = a - delta
+        if is_part_two:
+            res.add(b)
+        while anti.real >= 0 and anti.real < width and anti.imag <= 0 and anti.imag > -height:
+            res.add(anti)
+            if not is_part_two:
+                break
+            anti -= delta
+    return res
+
 if __name__ == "__main__":
     args = _parse_args()
     t = time()
+    i = 0
+    data = {}
     with Path(f"inputs/{Path(__file__).stem}.txt").open("r") as file:
-        data = file.read()
-    if args.part == 1:
-        print(data)
-    else:
-        raise NotImplementedError
+        while line := file.readline():
+            for r, char in enumerate(line.strip()):
+                if char != ".":
+                    data[char] = data.get(char, set()) | {complex(r, -i)}
+                width = r + 1
+            i += 1
+        height = i
+    antinodes = set()
+    for antennas in data.values():
+        antinodes |= locate_antinodes(antennas, is_part_two=args.part == 2)
+    print(len(antinodes))
     print(time() - t)
