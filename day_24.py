848cf63686a8119081ab74f975be261e2865e12e
Andréa <andrea.marnier@gmail.com>
2024-12-29 23:35:50 +0100

    day 24 update

--- day_24.py
+++ day_24.py
@@ -43,6 +43,11 @@ def parse_inst(raw: str) -> list[Instruction]:
     for i, line in enumerate(raw.split("\n")):
         left, right = line.split(" -> ")
         op1, operator, op2 = left.split(" ")
+        if operator == "XOR":
+            if op1[0] not in "xy" and op2[0] not in "xy" and right[0] != "z":
+                to_switch.add(right)
+            if op1[0] in "xy" and op2[0] in "xy" and right[0] == "z" and right != "z00":
+                to_switch.add(right)
         res.append(Instruction(
             id=i,
             op1=op1,
@@ -66,13 +71,36 @@ def simulate() -> None:
             data[instruction.res] = instruction.action(op1, op2)
             seen.add(instruction.id)
 
-def read_register() -> int:
+def read_register(start: str) -> int:
     res = []
     for key, val in data.items():
-        if key.startswith("z"):
+        if key.startswith(start):
             res.append((int(key[1:]), str(val)))
     res.sort(reverse=True)
     return int("".join([x[1] for x in res]), 2)
+
+def find_swap(op: str) -> str:
+    for inst in instructions:
+        if inst.op1 == op or inst.op2 == op:
+            if inst.res.startswith("z"):
+                return "z{:02d}".format(int(inst.res[1:]) - 1)
+            else:
+                return find_swap(inst.res)
+
+def perform_swap() -> None:
+    for res1, res2 in swap_pairs:
+        for inst in instructions:
+            if inst.res == res1:
+                inst.res = res2
+            elif inst.res == res2:
+                inst.res = res1
+
+def find_missing(idx: int) -> tuple:
+    return tuple([
+        inst.res
+        for inst in instructions
+        if inst.op1[1:].isdigit() and inst.op2[1:].isdigit() and int(inst.op1[1:])==idx and int(inst.op2[1::])
+    ])
 
 if __name__ == "__main__":
     args = _parse_args()
@@ -80,10 +108,11 @@ if __name__ == "__main__":
     with Path(f"inputs/{Path(__file__).stem}.txt").open("r") as file:
         init, inst = file.read().split("\n\n")
     data = parse_init(init)
+    to_switch = set()
     instructions = parse_inst(inst.strip())
     if args.part == 1:
         simulate()
-        print(read_register())
+        print(read_register("z"))
     else:
         # print(bin(x + y - z)) and search for errors
         # Found 3 pairs to swap that way:
@@ -94,6 +123,14 @@ if __name__ == "__main__":
         # Someone suggested to change the input, so I tried with x=17592186044415 (only 1s)  and z=0.
         # Found the last swap with same method as previously (which was just the next check I was supposed to make)
         # gqp, z33
-        # Then:
-        print(",".join(sorted(["mwk", "z10", "z18", "qgd", "hsw", "jmh", "gqp", "z33"])))
+        # Then print(",".join(sorted(["mwk", "z10", "z18", "qgd", "hsw", "jmh", "gqp", "z33"])))
+
+        # Later tried the programmatic approach thanks to this post: https://shorturl.at/espZf
+        swap_pairs = [(item, find_swap(item)) for item in to_switch]
+        perform_swap()
+        simulate()
+        delta = read_register("x") + read_register("y") - read_register("z")
+        diff_bit = bin(delta)[2 if delta else 3:][::-1].index('1')
+        swap_pairs.extend([find_missing(diff_bit)])
+        print(",".join(sorted([item for pair in swap_pairs for item in pair])))
     print(time() - t)
