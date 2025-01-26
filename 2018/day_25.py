9c27027aaf522d06cd82dc173a54641c954d8e89
Andréa <andrea.marnier@gmail.com>
2025-01-26 22:15:08 +0100

    2018 day 25

---
+++ 2018/day_25.py
@@ -0,0 +1,56 @@
+import argparse
+
+from pathlib import Path
+from time import time
+
+def _parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--part", "-p",
+        type=int,
+        choices={1, 2},
+        help="Set puzzle part"
+    )
+    args = parser.parse_args()
+    if not args.part:
+        parser.error("Which part are you solving?")
+    return args
+
+def distance(t1: tuple, t2: tuple) -> int:
+    return sum(abs(t1[i] - t2[i]) for i in range(4))
+
+def is_in_same_constellation(t1: tuple, t2: tuple) -> bool:
+    return distance(t1, t2) <= 3
+
+if __name__ == "__main__":
+    args = _parse_args()
+    t = time()
+    with Path(f"inputs/{Path(__file__).stem}.txt").open("r") as file:
+        data = [tuple([int(x) for x in line.split(",")]) for line in file.read().strip().split("\n")]
+    if args.part == 1:
+        constellations = {}
+        const_key = 1
+        to_del = set()
+        for coord in data:
+            linked = 0
+            for k, constellation in constellations.items():
+                if k in to_del:
+                    continue
+                for star in constellation:
+                    if is_in_same_constellation(star, coord):
+                        if linked:
+                            constellation |= constellations[linked]
+                            to_del.add(linked)
+                            linked = k
+                            break
+                        else:
+                            constellation.add(coord)
+                            linked = k
+                            break
+            if not linked:
+                constellations[const_key] = {coord}
+                const_key += 1
+        print(len(constellations.keys()) - len(to_del))
+    else:
+        raise NotImplementedError
+    print(time() - t)
--- README.md
+++ README.md
@@ -7,7 +7,7 @@ I participated to my first Advent of code in 2023. I loved it so much I decided 
 
 <!-- AOC TILES BEGIN -->
 <h1 align="center">
-  Advent of Code - 298/500 ⭐
+  Advent of Code - 300/500 ⭐
 </h1>
 <h1 align="center">
   2024 - 50 ⭐ - Python
@@ -166,7 +166,7 @@ I participated to my first Advent of code in 2023. I loved it so much I decided 
   <img src=".aoc_tiles/tiles/2023/25.png" width="161px">
 </a>
 <h1 align="center">
-  2018 - 48 ⭐ - Python
+  2018 - 50 ⭐ - Python
 </h1>
 <a href="2018/day_01.py">
   <img src=".aoc_tiles/tiles/2018/01.png" width="161px">
@@ -239,6 +239,9 @@ I participated to my first Advent of code in 2023. I loved it so much I decided 
 </a>
 <a href="2018/day_24.py">
   <img src=".aoc_tiles/tiles/2018/24.png" width="161px">
+</a>
+<a href="2018/day_25.py">
+  <img src=".aoc_tiles/tiles/2018/25.png" width="161px">
 </a>
 <h1 align="center">
   2017 - 50 ⭐ - Python
